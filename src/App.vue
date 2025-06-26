ed0e55e7b3e7b53b99b0fcf07b32cfec692e2e71
Hoopsy <renanhoruz@gmail.com>
2025-06-26 23:58:25 +0300

    the backbone

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,92 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <MyLinks />
+  <img alt="Vue logo" src="./assets/Dealmaker.png" style="height:128px;width: 128px;">
+  <HelloWorld msg="Hi hiii!" />
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import MyLinks from './components/Links.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HelloWorld,
+    MyLinks
   }
 }
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: 'Mononoki Nerd Font';
+  display: flex;
+  flex-direction: column;
+  margin-left: 5%;
+  width: 90%;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+  border: solid;
+  border-radius: 15px;
+  border-color: #000000;
+  background-color: #f3eab0;
+}
+
+
+img {
+  align-self: center;
+  border: solid;
+  border-radius: 15px;
+  border-width: 2px;
+  image-rendering: optimizeSpeed;
+  /* STOP SMOOTHING, GIVE ME SPEED  */
+  image-rendering: -moz-crisp-edges;
+  /* Firefox                        */
+  image-rendering: -o-crisp-edges;
+  /* Opera                          */
+  image-rendering: -webkit-optimize-contrast;
+  /* Chrome (and eventually Safari) */
+  image-rendering: pixelated;
+  /* Universal support since 2021   */
+  image-rendering: optimize-contrast;
+  /* CSS3 Proposed                  */
+  -ms-interpolation-mode: nearest-neighbor;
+  /* IE8+                           */
+
+}
+
+/* Regular */
+@font-face {
+  font-family: 'Mononoki Nerd Font';
+  src: "./assets/Mononoki/MononokiNerdFont-Regular.ttf')";
+  font-weight: normal;
+  font-style: normal;
+}
+
+/* Bold */
+@font-face {
+  font-family: 'Mononoki Nerd Font';
+  src: "./assets/Mononoki/MononokiNerdFont-Bold.ttf";
+  font-weight: bold;
+  font-style: normal;
+}
+
+/* Italic */
+@font-face {
+  font-family: 'Mononoki Nerd Font';
+  src: "./assets/Mononoki/MononokiNerdFont-Italic.ttf')";
+  font-weight: normal;
+  font-style: italic;
+}
+
+/* Bold Italic */
+@font-face {
+  font-family: 'Mononoki Nerd Font';
+  src: "./assets/Mononoki/MononokiNerdFont-BoldItalic.ttf";
+  font-weight: bold;
+  font-style: italic;
 }
 </style>
---
+++ src/components/Links.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="flex">
+    <a href="https://www.youtube.com/@HoopsyMakesGames" class="youtube fa fa-youtube"></a>
+    <a href="https://www.twitch.tv/hoopsymakes" class="fa fa-twitch twitch"></a>
+    <a href="https://ko-fi.com/hoopsy" class="fa fa-kofi">kofi</a>
+    <a href="https://github.com/Hoopsier" class='fa fa-github github'></a>
+    <a href="https://www.linkedin.com/in/renan-horuz-99a41a28b/" class="fa fa-linkedin linkedin"></a>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'MyLinks',
+  props: {
+    youtube: String,
+    github: String,
+    twitch: String,
+    linkedin: String
+  }
+}
+</script>
+
+<style scoped>
+.flex {
+  display: flex;
+}
+</style>
+<style scoped>
+div {
+  margin-top: 2px;
+  direction: rtl;
+}
+
+a {
+  margin-right: 5px;
+  font-size: 24px;
+  color: #e9c460;
+  text-decoration: none;
+}
+</style>
